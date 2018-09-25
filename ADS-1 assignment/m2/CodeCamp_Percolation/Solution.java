92c11ef4bd49c5994724519da6cfec5948ec345a
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-25 20:30:22 +0530

    "check_style_score -> 12 of 24 passed. style score: 0/1 "

--- ADS-1 assignment/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1 assignment/m2/CodeCamp_Percolation/Solution.java
@@ -46,17 +46,17 @@ final class Percolation {
         if (row == n - 1) {
             wqu.union(component(row, col), ((n * n) + 1));
         }
-        if (row > 0 && grid[row][col] == 1) {
-            wqu.union(component(row, col), component(row + 1, col));
-        }
-        if (row < n - 1 && grid[row + 1][col] == 1) {
+        if (row > 0 && grid[row - 1][col] == 1) {
             wqu.union(component(row, col), component(row - 1, col));
         }
-        if (col > 0 && grid[row][col] == 1) {
-            wqu.union(component(row, col), component(row, col + 1));
+        if (row < n - 1 && grid[row + 1][col] == 1) {
+            wqu.union(component(row, col), component(row + 1, col));
+        }
+        if (col > 0 && grid[row][col - 1] == 1) {
+            wqu.union(component(row, col), component(row, col - 1));
         }
         if (col < n - 1 && grid[row][col + 1] == 1) {
-            wqu.union(component(row, col), component(row, col - 1));
+            wqu.union(component(row, col), component(row, col + 1));
         }
     }
     /**
