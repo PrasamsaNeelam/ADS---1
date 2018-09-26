dfc89f22565a2aeb51fde5d2a5de392ad287456a
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-26 18:51:21 +0530

    no.of pairs

--- ADS-1 practice/m3/No_of_pairs/Solution.java
+++ ADS-1 practice/m3/No_of_pairs/Solution.java
@@ -8,7 +8,8 @@ public class Solution {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the size of array");
 		int size = s.nextInt();
-		int count = 0;
+		int count = 1;
+		int sum = 0;
 		int[] arr = new int[size];
 		System.out.println("Enter the elements");
 		for (int i = 0; i < arr.length; i++) {
@@ -16,11 +17,22 @@ public class Solution {
 		}
 		Arrays.sort(arr);
 		System.out.println(Arrays.toString(arr));
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] == arr[i - 1]) {
+		for (int i = 0; i < size - 1; i++) {
+			if (i == size - 2 && arr[i] == arr[i + 1]) {
 				count++;
+				sum += count * (count - 1) / 2;
+				count = 1;
+				break;
+			}
+			if (arr[i] == arr[i + 1]) {
+				count++;
+				continue;
+			}
+			if (count > 1 && arr[i] != arr[i + 1]) {
+				sum += count * (count - 1) / 2;
+				count = 1;
 			}
 		}
-		System.out.println("count is " + count);
-}
+		System.out.println("count is " + sum);
+	}
 }
