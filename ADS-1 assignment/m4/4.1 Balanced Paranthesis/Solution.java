eed6812e4a5db310d2f324c0ab25a6196715e6c4
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-27 23:04:15 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1 assignment/m4/4.1 Balanced Paranthesis/Solution.java
+++ ADS-1 assignment/m4/4.1 Balanced Paranthesis/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Class for node.
  */
@@ -99,5 +100,32 @@ class Balanced {
             }
         }
         return ll.gethead() == null;
+    }
+}
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Empty constructor.
+    }
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        while (n != 0) {
+            String[] input = sc.nextLine().split("");
+            Balanced b = new Balanced();
+            if (b.balancing(input)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            n--;
+        }
     }
 }
