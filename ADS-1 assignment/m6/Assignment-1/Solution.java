fe4a981e598eaff294ac9144080115ae0fdf4ca0
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-29 12:46:22 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-1 assignment/m6/Assignment-1/Solution.java
+++ ADS-1 assignment/m6/Assignment-1/Solution.java
@@ -9,31 +9,36 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { main function to read the inputs and methods }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String input = sc.nextLine();
-		String p = sc.nextLine();
-		String q = sc.nextLine();
-		switch (input) {
-		case "numberToDigits":
-			LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
-			LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
-			System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-			System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-			break;
+    Solution() {
 
-			// case "addLargeNumbers":
-			// 	pDigits = AddLargeNumbers.numberToDigits(p);
-			// 	qDigits = AddLargeNumbers.numberToDigits(q);
-			// 	LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-			// 	System.out.println(AddLargeNumbers.digitsToNumber(result));
-			// 	break;
-		}
-	}
+    }
+    /**
+     * { main function to read the inputs and methods }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch (input) {
+        case "numberToDigits":
+            LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+            LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+            break;
+
+            // case "addLargeNumbers":
+            //  pDigits = AddLargeNumbers.numberToDigits(p);
+            //  qDigits = AddLargeNumbers.numberToDigits(q);
+            //  LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+            //  System.out.println(AddLargeNumbers.digitsToNumber(result));
+            //  break;
+        default:
+            break;
+        }
+    }
 
 }
