e414769dd62bbd890a785f1a049b4ad9c7cb4e1e
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-29 12:40:27 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

--- ADS-1 assignment/m6/Assignment-1/Solution.java
+++ ADS-1 assignment/m6/Assignment-1/Solution.java
@@ -1,25 +1,52 @@
+/**
+ * { imports Scanner class }.
+ * @author : Prasamsa
+ * Date: 29 Septemeber, 2018
+ */
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
 	String data;
 	Node next;
+	/**
+	 * Constructs the object.
+	 */
 	Node() {
 
 	}
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      data  The data
+	 */
 	Node(String data) {
 		this.data = data;
 		next = null;
 	}
 }
+/**
+ * List of linkeds.
+ */
 class LinkedList {
 	Node first;
 	Node last;
 	int size;
+	/**
+	 * Constructs the object.
+	 */
 	LinkedList() {
 		first = new Node();
 		last = new Node();
 		size = 0;
 	}
-	void insert(String item) {
+	/**
+	 * { function to insert an item }.
+	 *
+	 * @param      item  The item
+	 */
+	public void insert(final String item) {
 		Node object = new Node(item);
 		if (size == 0) {
 			first = object;
@@ -31,49 +58,97 @@ class LinkedList {
 		last = object;
 		size++;
 	}
-	String delete() {
+	/**
+	 * { function to delete an element }.
+	 *
+	 * @return     { returns String }
+	 */
+	public String delete() {
 		String data = last.data;
 		last = last.next;
 		size--;
 		return data;
 	}
-	boolean isEmpty() {
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
+	public boolean isEmpty() {
 		if (size == 0) {
 			return true;
 		}
 		return false;
 	}
-	int size() {
+	/**
+	 * { function to keep track of size }.
+	 *
+	 * @return     { returns int type }
+	 */
+	public int size() {
 		return size;
 	}
 }
+/**
+ * List of .
+ */
 class Stack {
 	LinkedList ll ;
+	/**
+	 * Constructs the object.
+	 */
 	Stack () {
 		ll = new LinkedList();
 	}
-
-	void push(String item) {
+	/**
+	 * { function to push an element into stack }.
+	 *
+	 * @param      item  The item
+	 */
+	public void push(String item) {
 		ll.insert(item);
 	}
-	String pop() {
+	/**
+	 * { function to pop an element from stack }.
+	 *
+	 * @return     { returns String type }
+	 */
+	public String pop() {
 		if (isEmpty()) {
 			System.out.println("Stack is empty");
 			return null;
 		}
 		return ll.delete();
 	}
-	boolean isEmpty() {
+	/**
+	 * Determines if empty.
+	 *
+	 * @return     True if empty, False otherwise.
+	 */
+	public boolean isEmpty() {
 		return ll.isEmpty();
 	}
+	/**
+	 * { function to keep ttrack of size }.
+	 *
+	 * @return     { returns int type }
+	 */
 	int size() {
 		return ll.size();
 	}
 }
-
+/**
+ * Class for add large numbers.
+ */
 class AddLargeNumbers {
-
-	public static LinkedList numberToDigits(String number) {
+	/**
+	 * { function to insert the numbers }.
+	 *
+	 * @param      number  The number
+	 *
+	 * @return     { returns LinkedList type }
+	 */
+	public static LinkedList numberToDigits(final String number) {
 		LinkedList obj = new LinkedList();
 		for (int i = 0; i < number.length(); i++) {
 			String c = number.charAt(i)+ "";
@@ -82,8 +157,14 @@ class AddLargeNumbers {
 		return obj;
 
 	}
-
-	public static String digitsToNumber(LinkedList list) {
+	/**
+	 * { function to append the values of each node into String }.
+	 *
+	 * @param      list  The list
+	 *
+	 * @return     { returns String type }
+	 */
+	public static String digitsToNumber(final LinkedList list) {
 		String str = "";
 		Node node = list.first;
 		for (int i = 0; i < list.size; i++) {
@@ -96,9 +177,16 @@ class AddLargeNumbers {
 
 	// }
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * { main function to read the inputs and methods }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 		String p = sc.nextLine();
