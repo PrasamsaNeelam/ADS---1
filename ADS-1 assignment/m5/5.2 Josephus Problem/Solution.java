0d9059a819f4e0fcc8524a72aabd7558c05a11cf
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-30 23:06:12 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- ADS-1 assignment/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1 assignment/m5/5.2 Josephus Problem/Solution.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
-        
+
     }
     /**
      * { main function to drive the program }.
@@ -29,7 +32,8 @@ public class Solution {
      * @param      queue   The queue
      * @param      people  The people
      */
-    public static void createCircle(final Deque<Integer> queue, final int people) {
+    public static void createCircle(final Deque<Integer> queue,
+        final int people) {
         for (int i = 0; i < people; i++) {
             queue.push(i);
         }
