9a84fdc92342740a788bd2e5754353c3c5019c04
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-30 22:29:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ ADS-1 assignment/m5/5.1 Deque/Solution.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+class Deque<Item> {
+	Node first = null;
+	Node last = null;
+	int size = 0;
+	class Node {
+		Item data;
+		Node next;
+		Node(Item data) {
+			this.data = data;
+		}
+	}
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void pushRight(Item element) {
+		Node oldleft = last;
+		last = new Node(element);
+		if (isEmpty()) {
+			first = last;
+			last.next = null;
+			size++;
+			printList();
+			return;
+		}
+		last.next = null;
+		oldleft.next = last;
+		size++;
+		printList();
+	}
+	public void pushLeft(Item element) {
+		Node oldright = first;
+		first = new Node(element);
+		if (isEmpty()) {
+			last = first;
+
+			size++;
+			printList();
+			return;
+
+		}
+		first.next = oldright;
+		size++;
+		printList();
+	}
+	public Item popLeft() {
+		if (size == 0) {
+			System.out.println("Deck is empty");
+			return null;
+		}
+		Item d = first.data;
+		if (isEmpty()) {
+			return null;
+		}
+		first = first.next;
+		size--;
+		printList();
+		return d;
+
+	}
+	public Item popRight() {
+		if (size == 0) {
+			System.out.println("Deck is empty");
+			return null;
+		}
+		Item d  =  last.data;
+		if (isEmpty()) {
+			return null;
+		}
+		int s = 0;
+		Node get = first;
+
+		while (s < size - 2) {
+			//System.out.println("herre" + size);
+			get = get.next;
+			//System.out.println(get.data);
+
+			s++;
+		}
+		last = get;
+		get.next = null;
+		size--;
+		printList();
+		return d;
+
+
+	}
+	public void printList() {
+		if (size == 0) {
+			System.out.println("[]");
+			return;
+		}
+		Node tnode = first;
+		String s = "";
+		while (tnode != null) {
+			s += tnode.data + ",";
+			tnode = tnode.next;
+		}
+		String[] str = s.split(",");
+		String pr = "[";
+		for(int i = 0;i<size-1;i++) {
+			pr += str[i] + ", ";
+		}
+		pr += str[size-1] + "]";
+		System.out.println(pr);
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Deque<Integer> queue = new Deque<>();
+		Scanner s = new Scanner(System.in);
+		int num = Integer.parseInt(s.nextLine());
+		while (s.hasNext()) {
+			String[] tokens = s.nextLine().split(" ");
+			switch (tokens[0]) {
+			case "pushLeft":
+				queue.pushLeft(Integer.parseInt(tokens[1]));
+				break;
+			case "pushRight":
+				queue.pushRight(Integer.parseInt(tokens[1]));
+				break;
+			case "size":
+				System.out.println(queue.getSize());
+				break;
+			case "popLeft":
+				queue.popLeft();
+				break;
+			case "popRight":
+				queue.popRight();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
