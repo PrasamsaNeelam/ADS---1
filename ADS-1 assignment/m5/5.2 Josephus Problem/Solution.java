5b4df1163fe1a696999685993fb7211f544c5feb
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-30 23:03:29 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1 assignment/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1 assignment/m5/5.2 Josephus Problem/Solution.java
@@ -3,32 +3,35 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { main function to drive the program }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		String data = s.nextLine();
-		while (s.hasNext()) {
-			String[] line = s.nextLine().split(" ");
-			int people = Integer.parseInt(line[0]);
-			int gap = Integer.parseInt(line[1]);
-			Deque<Integer> queue = new Deque<>();
-			createCircle(queue, people);
-			queue.josephus(people, gap);
-		}
-	}
-	/**
-	 * Creates a circle.
-	 *
-	 * @param      queue   The queue
-	 * @param      people  The people
-	 */
-	public static void createCircle(Deque<Integer> queue, int people) {
-		for (int i = 0; i < people; i++) {
-			queue.push(i);
-		}
-	}
+    private Solution() {
+        
+    }
+    /**
+     * { main function to drive the program }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String data = s.nextLine();
+        while (s.hasNext()) {
+            String[] line = s.nextLine().split(" ");
+            int people = Integer.parseInt(line[0]);
+            int gap = Integer.parseInt(line[1]);
+            Deque<Integer> queue = new Deque<>();
+            createCircle(queue, people);
+            queue.josephus(people, gap);
+        }
+    }
+    /**
+     * Creates a circle.
+     *
+     * @param      queue   The queue
+     * @param      people  The people
+     */
+    public static void createCircle(final Deque<Integer> queue, final int people) {
+        for (int i = 0; i < people; i++) {
+            queue.push(i);
+        }
+    }
 }
