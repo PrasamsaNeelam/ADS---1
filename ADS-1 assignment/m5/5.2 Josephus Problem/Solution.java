b0521555b6b5f4e688921c635f895e51ab00a3c2
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-09-30 22:59:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS-1 assignment/m5/5.2 Josephus Problem/Solution.java
+++ ADS-1 assignment/m5/5.2 Josephus Problem/Solution.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
+	/**
+	 * { main function to drive the program }.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String data = s.nextLine();
@@ -15,107 +20,15 @@ public class Solution {
 			queue.josephus(people, gap);
 		}
 	}
-
+	/**
+	 * Creates a circle.
+	 *
+	 * @param      queue   The queue
+	 * @param      people  The people
+	 */
 	public static void createCircle(Deque<Integer> queue, int people) {
 		for (int i = 0; i < people; i++) {
 			queue.push(i);
 		}
 	}
-}
-class Deque<Item> {
-	Node first = null;
-	Node last = null;
-	int size = 0;
-	String print = "";
-	class Node {
-		Item data;
-		Node next;
-		Node(Item data) {
-			this.data = data;
-		}
-	}
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	public int getSize() {
-		return size;
-	}
-	public void push(Item element) {
-		Node oldleft = last;
-		last = new Node(element);
-		if (isEmpty()) {
-			first = last;
-			last.next = first;
-			size++;
-			return;
-		}
-		last.next = first;
-		oldleft.next = last;
-		size++;
-	}
-
-	public Item pop(Item person) {
-		Item d = first.data;
-		if (isEmpty()) {
-			return null;
-		}
-		int s = 0;
-		Node get = first;
-		Node prev = null;
-		while (s < size) {
-			if (get.data == person) {
-				if (first.data == person) {
-					first = first.next;
-					int g = 0;
-					Node l = first;
-					while (g < size - 2) {
-						l = l.next;
-
-						g++;
-					}
-					last = l;
-					last.next = first;
-				} else {
-					prev.next = get.next;
-				}
-				size--;
-				break;
-			}
-			prev = get;
-			get = get.next;
-			s++;
-		}
-		return d;
-	}
-
-	public void josephus(int people, int gap) {
-
-		Node head = first;
-		while (!isEmpty()) {
-			int c = 0;
-			while (c < gap - 1) {
-				head = head.next;
-				c++;
-			}
-			print += head.data+" ";
-			pop(head.data);
-			head = head.next;
-		}
-		print = print.trim();
-		System.out.println(print);
-
-	}
-	public void printList() {
-		Node n = first;
-		Node tnode = first.next;
-		String s = "";
-		s += n.data + " ";
-		while (tnode != first) {
-			s += tnode.data + " ";
-			tnode = tnode.next;
-		}
-		s = s.trim();
-		System.out.println(s);
-	}
-
 }
