a0d50c2fdcf682ce4a5eccb3c3ad8d8d9e968e76
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-01 17:43:12 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- ADS-1 assignment/m7/7.1 Steque/Solution.java
+++ ADS-1 assignment/m7/7.1 Steque/Solution.java
@@ -75,7 +75,7 @@ public class Solution {
 	Solution() {
 
 	}
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int num = Integer.parseInt(s.nextLine());
 		Steque sq = new Steque();
