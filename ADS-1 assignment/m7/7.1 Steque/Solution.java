25a075c50381b79cfc533b4b9c53e89f40a0f2a1
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-01 17:42:44 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS-1 assignment/m7/7.1 Steque/Solution.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+class Node {
+	String data;
+	Node next;
+	Node() {
+
+	}
+	Node(String data) {
+		this.data = data;
+		next = null;
+	}
+}
+class Steque {
+	Node start;
+	Node temp;
+	Node end;
+	int size;
+	Steque() {
+		start = new Node();
+		temp = new Node();
+		end = new Node();
+		size = 0;
+	}
+	void push(String item) {
+		Node obj = new Node(item);
+		if (size == 0) {
+			start = obj;
+			end = start;
+			size++;
+			return;
+		}
+		obj.next = start;
+		start = obj;
+		size++;
+	}
+	void enqueue(String item) {
+		Node obj = new Node();
+		if (size == 0) {
+			start = obj;
+			end = obj;
+			size++;
+			return;
+		}
+		end.next = obj;
+		end = obj;
+		size++;
+	}
+	String pop() throws Exception {
+		String data = start.data; 
+		start = start.next;
+		size--;
+		return data;
+	}
+	boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+	int size() {
+		return size;
+	}
+	public String toString() {
+		temp = start;
+		String out = "";
+		while(temp.next != null) {
+			out += temp.data + ", ";
+			temp = temp.next;
+		}
+		return out.substring(0, out.length() - 2);
+	}
+}
+
+public class Solution {
+	Solution() {
+
+	}
+	public void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int num = Integer.parseInt(s.nextLine());
+		Steque sq = new Steque();
+		while (s.hasNext()) {
+			String[] tokens = s.nextLine().split(" ");
+			switch(tokens[0]) {
+				case "push":
+				sq.push(tokens[1]);
+				System.out.println(sq);
+				break;
+				case "pop":
+				try {
+					sq.pop();
+					System.out.println(sq);
+				} catch(Exception e) {
+
+				}
+				break;
+				case "enqueue":
+				sq.enqueue(tokens[1]);
+				System.out.println(sq);
+				break;
+			}
+		}
+	}
+}
