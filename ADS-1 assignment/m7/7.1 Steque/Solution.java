a7a9e2a19ec3b727b2faa43d5f9e849c773255bf
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-01 20:02:26 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1 assignment/m7/7.1 Steque/Solution.java
+++ ADS-1 assignment/m7/7.1 Steque/Solution.java
@@ -1,115 +1,52 @@
+/**
+ * @author : Prasamsa.
+ * date : 1st October,2018.
+ */
 import java.util.Scanner;
-class Node {
-	String data;
-	Node next;
-	Node() {
 
-	}
-	Node(String data) {
-		this.data = data;
-		next = null;
-	}
-}
-class Steque {
-	Node start;
-	Node temp;
-	Node end;
-	int size;
-	Steque() {
-		start = new Node();
-		temp = new Node();
-		end = new Node();
-		size = 0;
-	}
-	void push(String item) {
-		Node obj = new Node(item);
-		if (size == 0) {
-			start = obj;
-			end = start;
-			size++;
-			return;
-		}
-		obj.next = start;
-		start = obj;
-		size++;
-	}
-	void enqueue(String item) {
-		Node obj = new Node(item);
-		if (size == 0) {
-			start = obj;
-			end = start;
-			size++;
-			return;
-		}
-		end.next = obj;
-		end = obj;
-		size++;
-	}
-	String pop() throws Exception {
-		if (size == 0) {
-			throw new Exception("Steque is empty.");
-		}
-		String data = start.data;
-		start = start.next;
-		size--;
-		return data;
-	}
-	boolean isEmpty() {
-		if (size == 0) {
-			return true;
-		}
-		return false;
-	}
-	int size() {
-		return size;
-	}
-	public String toString() {
-		if (size == 0) {
-			//System.out.println("Steque is empty.");
-			return  "Steque is empty.";
-		}
-		temp = start;
-		String out = "";
-		while(temp != null) {
-			out += temp.data + ", ";
-			temp = temp.next;
-		}
-		return out.substring(0, out.length() - 2);
-	}
-}
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int num = Integer.parseInt(s.nextLine());
-		Steque sq = new Steque();
-		while (s.hasNext()) {
-			String[] tokens = s.nextLine().split(" ");
-			switch(tokens[0]) {
-				case "push":
-				sq.push(tokens[1]);
-				System.out.println(sq);
-				break;
-				case "pop":
-				try {
-					sq.pop();
-					System.out.println(sq);
-				} catch(Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-				case "enqueue":
-				sq.enqueue(tokens[1]);
-				System.out.println(sq);
-				break;
-				default:
-				sq = new Steque();
-				System.out.println();
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = Integer.parseInt(s.nextLine());
+        Steque sq = new Steque();
+        while (s.hasNext()) {
+            String[] tokens = s.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "push":
+                sq.push(tokens[1]);
+                System.out.println(sq);
+                break;
+            case "pop":
+                try {
+                    sq.pop();
+                    System.out.println(sq);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "enqueue":
+                sq.enqueue(tokens[1]);
+                System.out.println(sq);
+                break;
+            default:
+                sq = new Steque();
+                System.out.println();
+                break;
+            }
+        }
+    }
 }
---
+++ ADS-1 assignment/m7/7.1 Steque/Steque.java
@@ -0,0 +1,143 @@
+/**
+ * Class for node.
+ */
+class Node {
+    /**
+     * { variable to access data }.
+     */
+    String data;
+    /**
+     * { var to access the next node }.
+     */
+    Node next;
+    /**
+     * Constructs the object.
+     */
+    Node() {
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      data  The data
+     */
+    Node(final String d) {
+        this.data = d;
+        next = null;
+    }
+}
+/**
+ * Class for steque.
+ */
+public class Steque {
+    /**
+     * { var for start node }.
+     */
+    Node start;
+    /**
+     * { var for temp node}.
+     */
+    Node temp;
+    /**
+     * { var for end node }.
+     */
+    Node end;
+    /**
+     * { var for size }.
+     */
+    int size;
+    /**
+     * Constructs the object.
+     */
+    Steque() {
+        start = new Node();
+        temp = new Node();
+        end = new Node();
+        size = 0;
+    }
+    /**
+     * { pushes the node at the left of List }.
+     *
+     * @param      item  The item
+     */
+    void push(final String item) {
+        Node obj = new Node(item);
+        if (size == 0) {
+            start = obj;
+            end = start;
+            size++;
+            return;
+        }
+        obj.next = start;
+        start = obj;
+        size++;
+    }
+    /**
+     * { add the node at the right of the List }.
+     *
+     * @param      item  The item
+     */
+    void enqueue(final String item) {
+        Node obj = new Node(item);
+        if (size == 0) {
+            start = obj;
+            end = start;
+            size++;
+            return;
+        }
+        end.next = obj;
+        end = obj;
+        size++;
+    }
+    /**
+     * { pops the node from the left }.
+     *
+     * @return     { description_of_the_return_value }
+     *
+     * @throws     Exception  { exception_description }
+     */
+    String pop() throws Exception {
+        if (size == 0) {
+            throw new Exception("Steque is empty.");
+        }
+        String data = start.data;
+        start = start.next;
+        size--;
+        return data;
+    }
+    /**
+     * Determines if empty.
+     *
+     * @return     True if empty, False otherwise.
+     */
+    boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * { returns size }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int size() {
+        return size;
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        if (size == 0) {
+            return "Steque is empty.";
+        }
+        temp = start;
+        String out = "";
+        while (temp != null) {
+            out += temp.data + ", ";
+            temp = temp.next;
+        }
+        return out.substring(0, out.length() - 2);
+    }
+}
