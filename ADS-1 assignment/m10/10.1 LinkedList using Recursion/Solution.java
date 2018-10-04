646baee823a6b279e9aa4f9ebb1dc8e4404aceab
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-04 22:52:53 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignment/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS-1 assignment/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,85 +1,4 @@
 import java.util.Scanner;
-class Node {
-	String data;
-	Node next;
-	Node(String data) {
-		this.data = data;
-	}
-}
-class LinkedList {
-	Node start;
-	int size;
-	LinkedList() {
-		start = null;
-		size = 0;
-	}
-	// time complexity for this method is n
-    //takes O(n) time complexity
-	void insertAt(int index, String element) throws Exception {
-		
-		if (index < 0 || index > size) {
-			throw new Exception();
-		}
-		Node obj = new Node(element);
-		// Node previous = null;
-		// Node temp = start;
-		// int count = 0;
-		// if (index == 0) {
-		// 	obj.next = start;
-		// 	start = obj;
-		// 	size++;
-		// 	return;
-		// }
-		start = insertAt(index, start, obj, 0);
-		// while (temp != null) {
-		// 	if (count == index) {
-		// 		previous.next = obj;
-		// 		obj.next = temp;
-		// 		size++;
-		// 		return;
-		// 	}
-		// 	previous = temp;
-		// 	temp = temp.next;
-		// 	count++;
-		// }
-	}
-	// time complexity for this method is n
-    //takes O(n) time complexity
-	Node insertAt(int index, Node first, Node obj, int count) throws Exception {
-		if (index == count) {
-			obj.next = first;
-			size++;
-			return obj;
-		}
-		first.next = insertAt(index, first.next, obj, count + 1);
-		return first;
-	}
-	// time complexity for this method is n
-    //takes O(n) time complexity
-	void reverse() {
-		reverse(null, start);
-	}
-	// time complexity for this method is n
-    //takes O(n) time complexity
-	void reverse(Node previous, Node current) {
-		if (current != null) {
-			reverse(current, current.next);
-			current.next = previous;
-		} else {
-			start = previous;
-		}
-	}
-	void print() {
-		String str = "";
-		Node temp = start;
-		while (temp != null) {
-			str += temp.data + ", ";
-			temp = temp.next;
-		}
-		System.out.println(str.substring(0, str.length() - 2));
-	}
-}
-
 /**
  * Class for solution.
  */
@@ -88,8 +7,8 @@ public final class Solution {
      * Constructs the object.
      */
    private Solution() {
+   	
     }
-
     /**
      * { main function }.
      *
