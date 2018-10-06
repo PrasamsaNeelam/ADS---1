5f766c06b4ff202a851bac31b2abd3eaba55c70f
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-06 12:59:15 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignment/m12/Assignment-1/Solution.java
+++ ADS-1 assignment/m12/Assignment-1/Solution.java
@@ -4,96 +4,116 @@
  * Date: 06-10-2018
  */
 import java.util.Scanner;
-import java.util.Comparator;
 /**
  * Class for student.
  */
-class Student {
-	private String studentName;
-	private String dob;
-	private Double sub1Marks;
-	private Double sub2Marks;
-	private Double sub3Marks;
-	private Double totalMarks;
-	private String reservation;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      name    The name
-	 * @param      dob1    The dob 1
-	 * @param      sub1    The sub 1
-	 * @param      sub2    The sub 2
-	 * @param      sub3    The sub 3
-	 * @param      marks   The marks
-	 * @param      reserv  The reserv
-	 */
-	Student(String name, String dob1, Double sub1, Double sub2, Double sub3, Double marks, String reserv) {
-		this.studentName = name;
-		this.dob = dob1;
-		this.sub1Marks = sub1;
-		this.sub2Marks = sub2;
-		this.sub3Marks = sub3;
-		this.totalMarks = marks;
-		this.reservation = reserv;
-	}
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      name    The name
-	 * @param      marks   The marks
-	 * @param      reserv  The reserv
-	 */
-	Student(String name, Double marks, String reserv) {
-		this.studentName = name;
-		this.totalMarks = marks;
-		this.reservation = reserv;
-	}
-	/**
-	 * Gets the name.
-	 *
-	 * @return     The name.
-	 */
-	public String getName() {
-		return this.studentName;
-	}
-	/**
-	 * Gets the dob.
-	 *
-	 * @return     The dob.
-	 */
-	public String getDob() {
-		return this.dob;
-	}
-	/**
-	 * Gets the marks.
-	 *
-	 * @return     The marks.
-	 */
-	public Double getMarks() {
-		return this.totalMarks;
-	}
-	/**
-	 * Gets the reserv.
-	 *
-	 * @return     The reserv.
-	 */
-	public String getReserv() {
-		return this.reservation;
-	}
-	// public int compareTo(Student std) {
- //    	if (!getMarks.equals())
+final class Student {
+    /**
+     * studentName.
+     */
+    private String studentName;
+    /**
+     * date of birth.
+     */
+    private String dob;
+    /**
+     * subject1 marks.
+     */
+    private Double sub1Marks;
+    /**
+     * subject2 marks.
+     */
+    private Double sub2Marks;
+    /**
+     * subject3 marks.
+     */
+    private Double sub3Marks;
+    /**
+     * total marks.
+     */
+    private Double totalMarks;
+    /**
+     * reservation.
+     */
+    private String reservation;
+    /**
+     * Constructs the object.
+     *
+     * @param      name    The name
+     * @param      dob1    The dob 1
+     * @param      sub1    The sub 1
+     * @param      sub2    The sub 2
+     * @param      sub3    The sub 3
+     * @param      marks   The marks
+     * @param      reserv  The reserv
+     */
+    Student(final String name, final String dob1, final Double sub1, final Double sub2, final Double sub3, final Double marks, final String reserv) {
+        this.studentName = name;
+        this.dob = dob1;
+        this.sub1Marks = sub1;
+        this.sub2Marks = sub2;
+        this.sub3Marks = sub3;
+        this.totalMarks = marks;
+        this.reservation = reserv;
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      name    The name
+     * @param      marks   The marks
+     * @param      reserv  The reserv
+     */
+    Student(final String name, final Double marks, final String reserv) {
+        this.studentName = name;
+        this.totalMarks = marks;
+        this.reservation = reserv;
+    }
+    /**
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.studentName;
+    }
+    /**
+     * Gets the dob.
+     *
+     * @return     The dob.
+     */
+    public String getDob() {
+        return this.dob;
+    }
+    /**
+     * Gets the marks.
+     *
+     * @return     The marks.
+     */
+    public Double getMarks() {
+        return this.totalMarks;
+    }
+    /**
+     * Gets the reserv.
+     *
+     * @return     The reserv.
+     */
+    public String getReserv() {
+        return this.reservation;
+    }
+    // public int compareTo(Student std) {
+ //     if (!getMarks.equals())
  //    }
-	public void addStudent(Student std) {
+    public void addStudent(Student std) {
 
-	}
+    }
 }
 /**
  * Class for insertion.
  */
-class Insertion {
-	/**
-	 * Constructs the object.
-	 */
+final class Insertion {
+    /**
+     * Constructs the object.
+     */
     private Insertion() { 
 
     }
@@ -104,7 +124,7 @@ class Insertion {
      * @param      lo    The lower
      * @param      hi    The higher
      */
-    public static void sort(Comparable[] a, int lo, int hi) {
+    public static void sort(final Comparable[] a, final int lo, final int hi) {
         for (int i = lo; i < hi; i++) {
             for (int j = i; j > lo && less(a[j], a[j-1]); j--) {
                 exch(a, j, j-1);
@@ -112,12 +132,12 @@ class Insertion {
         }
         assert isSorted(a, lo, hi);
     }
-    private static void exch(Object[] a, int i, int j) {
+    private static void exch(final Object[] a, final int i, final int j) {
         Object swap = a[i];
         a[i] = a[j];
         a[j] = swap;
     }
-    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+    private static boolean isSorted(final Comparable[] a, final int lo, final int hi) {
         for (int i = lo+1; i < hi; i++)
             if (less(a[i], a[i-1])) return false;
         return true;
@@ -131,7 +151,7 @@ class Insertion {
      *
      * @return     returns boolean.
      */
-    private static boolean less(Comparable v, Comparable w) {
+    private static boolean less(final Comparable v, final Comparable w) {
         return v.compareTo(w) < 0;
     }
 }
@@ -140,34 +160,34 @@ class Insertion {
     * Class for solution.
     */
 public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main function to drive the program.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int num = s.nextInt();
-		int vacancies = s.nextInt();
-		int noOfUnreserved = s.nextInt();
-		int noOfBC = s.nextInt();
-		int noOfSC = s.nextInt();
-		int noOfST = s.nextInt();
-		int counter = 0;
-		while (s.hasNext()) {
-			String[] arr = s.nextLine().split(",");
-			String[] dob = arr[1].split("-");
-			Student std = new Student(arr[0], arr[1], Double.parseDouble(arr[2]), Double.parseDouble(arr[3]), Double.parseDouble(arr[4]), Double.parseDouble(arr[4]), arr[6]);
-        	std.addStudent(std);
+    }
+    /**
+     * main function to drive the program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int vacancies = s.nextInt();
+        int noOfUnreserved = s.nextInt();
+        int noOfBC = s.nextInt();
+        int noOfSC = s.nextInt();
+        int noOfST = s.nextInt();
+        int counter = 0;
+        while (s.hasNext()) {
+            String[] arr = s.nextLine().split(",");
+            String[] dob = arr[1].split("-");
+            Student std = new Student(arr[0], arr[1], Double.parseDouble(arr[2]), Double.parseDouble(arr[3]), Double.parseDouble(arr[4]), Double.parseDouble(arr[4]), arr[6]);
+            std.addStudent(std);
         }
 
-		}
-	}
+        }
+    }
 
 
