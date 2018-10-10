e49c61c25fd03a9a619d73064dfc8ece8382a06f
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-10 20:35:16 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignment/m15/15.1 BinarySearchSymbolTable/Solution.java
+++ ADS-1 assignment/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -1,33 +1,67 @@
+/**
+ * @author : Prasamsa.
+ * Date : 10th October,2018.
+ */
 import java.util.Scanner;
 import java.util.NoSuchElementException;
-
+/**
+ * Class for binary search st.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
 class BinarySearchST<Key extends Comparable<Key>, Value> {
-	Key[] keys;
-	Value[] values;
-	int size;
-	BinarySearchST() {
-
-	}
-	BinarySearchST(int capacity) {
+	/**
+	 * array for keys.
+	 */
+	private Key[] keys;
+	/**
+	 * array for values.
+	 */
+	private Value[] values;
+	/**
+	 * variable for size.
+	 */
+	private int size;
+	/**
+	 * Constructs the object.
+	 */
+	BinarySearchST() { }
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      capacity  The capacity
+	 */
+	BinarySearchST(final int capacity) {
 		keys = (Key[]) new Comparable[capacity];
 		values = (Value[]) new Object[capacity];
 		size = 0;
 	}
-
-	private void resize(int capacity) {
-        Key[] tempkey = (Key[]) new Comparable[capacity];
-        Value[] tempval = (Value[]) new Object[capacity];
-        for (int i = 0; i < size; i++) {
-            tempkey[i] = keys[i];
-            tempval[i] = values[i];
-        }
-        keys = tempkey;
-        values = tempval;
-    }
-
-	void put(Key key, Value value) {
+	/**
+	 * resize function.
+	 *
+	 * @param      capacity  The capacity
+	 */
+	private void resize(final int capacity) {
+		Key[] tempkey = (Key[]) new Comparable[capacity];
+		Value[] tempval = (Value[]) new Object[capacity];
+		for (int i = 0; i < size; i++) {
+			tempkey[i] = keys[i];
+			tempval[i] = values[i];
+		}
+		keys = tempkey;
+		values = tempval;
+	}
+	/**
+	 * put function.
+	 *
+	 * @param      key    The key
+	 * @param      value  The value
+	 */
+	void put(final Key key, final Value value) {
 		if (key == null) {
-			throw new IllegalArgumentException("first argument to put() is null");
+			throw new IllegalArgumentException(
+			    "first argument to put() is null");
 		}
 		if (value == null) {
 			delete(key);
@@ -49,11 +83,17 @@ class BinarySearchST<Key extends Comparable<Key>, Value> {
 		values[r] = value;
 		size++;
 	}
-
-	int rank(Key key) {
+	/**
+	 * Rank Function.
+	 *
+	 * @param      key   The key
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	int rank(final Key key) {
 		if (key == null) {
-			throw new IllegalArgumentException("argument to rank() is null");	
-		}  
+			throw new IllegalArgumentException("argument to rank() is null");
+		}
 		int low = 0;
 		int high = size - 1;
 		while (low <= high) {
@@ -69,77 +109,124 @@ class BinarySearchST<Key extends Comparable<Key>, Value> {
 		}
 		return low;
 	}
-
-	boolean contains(Key key) {
+	/**
+	 * checks whether the key is present.
+	 *
+	 * @param      key   The key
+	 *
+	 * @return     { boolean }
+	 */
+	boolean contains(final Key key) {
 		return get(key) != null;
 	}
-
-	Value get(Key key) {
+	/**
+	 * Gets the key.
+	 *
+	 * @param      key   The key
+	 *
+	 * @return     { value }
+	 */
+	Value get(final Key key) {
 		if (key == null) {
-            throw new IllegalArgumentException("Key is null");
-        }
-        int r = rank(key);
-        if (r < size && keys[r].compareTo(key) == 0) {
-        	return values[r];
-        }
-        return null;
-    }
-
-    Key max() {
-    	return keys[size - 1];
-    }
-
-    Key min() {
-    	return keys[0];
-    }
-
-    Key floor(Key key) {
-    	if (key == null) {
-            throw new IllegalArgumentException("argument to floor() is null");
-        }
-    	int r = rank(key);
-    	if (r < size && keys[r].compareTo(key) == 0) {
-    		return key;
-    	}
-    	if(r==0){
-    		return null;
-    	}
-    	return keys[r - 1];
-    	}
-
-    void deleteMin() {
-    	delete(keys[0]);
-    }
-
+			throw new IllegalArgumentException("Key is null");
+		}
+		int r = rank(key);
+		if (r < size && keys[r].compareTo(key) == 0) {
+			return values[r];
+		}
+		return null;
+	}
+	/**
+	 * returns max key.
+	 *
+	 * @return     { Key }
+	 */
+	Key max() {
+		return keys[size - 1];
+	}
+	/**
+	 * returns min key.
+	 *
+	 * @return     { key }
+	 */
+	Key min() {
+		return keys[0];
+	}
+	/**
+	 *  return largest key less than or equal to key.
+	 *
+	 * @param      key   The key
+	 *
+	 * @return     { key }
+	 */
+	Key floor(final Key key) {
+		if (key == null) {
+			throw new IllegalArgumentException("argument to floor() is null");
+		}
+		int r = rank(key);
+		if (r < size && keys[r].compareTo(key) == 0) {
+			return key;
+		}
+		if (r == 0) {
+			return null;
+		}
+		return keys[r - 1];
+	}
+	/**
+	 * Delete minimum.
+	 */
+	void deleteMin() {
+		delete(keys[0]);
+	}
+	/**
+	 * returns size.
+	 *
+	 * @return     { int }
+	 */
 	int size() {
 		return size;
 	}
-
-	void delete(Key key) {
+	/**
+	 * deletes the key.
+	 *
+	 * @param      key   The key
+	 */
+	void delete(final Key key) {
 		int r = rank(key);
 		if (r == size || keys[r].compareTo(key) != 0) {
 			return;
 		}
 		for (int i = r; i < size - 1; i++) {
-			keys[i] = keys[i+ 1];
-			values[i] = values[i+1];
+			keys[i] = keys[i + 1];
+			values[i] = values[i + 1];
 		}
 		size--;
 		keys[size] = null;
 		values[size] = null;
 	}
-
+	/**
+	 * displays all keys.
+	 */
 	void keys() {
 		for (int i = 0; i < size; i++) {
-			System.out.println(keys[i] +" "+ values[i]);
+			System.out.println(keys[i] + " " + values[i]);
 		}
 	}
 }
-public class Solution {
-	private Solution() {
-
-	}
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() { }
+	/**
+	 * Main Function.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		String[] str = s.nextLine().split(" ");
 		BinarySearchST bs = new BinarySearchST<String, Integer>(str.length);
@@ -149,33 +236,33 @@ public class Solution {
 		while (s.hasNext()) {
 			String[] tokens = s.nextLine().split(" ");
 			switch (tokens[0]) {
-                case "contains":
-                System.out.println(bs.contains(tokens[1]));
-                break;
-                case "get":
-                System.out.println(bs.get(tokens[1]));
-                break;
-                case "max":
-                System.out.println(bs.max());
-                break;
-                case "min":
-                System.out.println(bs.min());
-                break;
-                case "floor":
-                System.out.println(bs.floor(tokens[1]));
-                break;
-                case "rank":
-                System.out.println(bs.rank(tokens[1]));
-                break;
-                case "deleteMin":
-                bs.deleteMin();
-                break;
-                case "keys":
-                bs.keys();
-                break;
-                default:
-                break;
+			case "contains":
+				System.out.println(bs.contains(tokens[1]));
+				break;
+			case "get":
+				System.out.println(bs.get(tokens[1]));
+				break;
+			case "max":
+				System.out.println(bs.max());
+				break;
+			case "min":
+				System.out.println(bs.min());
+				break;
+			case "floor":
+				System.out.println(bs.floor(tokens[1]));
+				break;
+			case "rank":
+				System.out.println(bs.rank(tokens[1]));
+				break;
+			case "deleteMin":
+				bs.deleteMin();
+				break;
+			case "keys":
+				bs.keys();
+				break;
+			default:
+				break;
+			}
 		}
 	}
-}
 }
