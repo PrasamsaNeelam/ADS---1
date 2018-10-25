6f2ca5b172bf3cb9e07572cd434a1137f3cf5059
PrasamsaNeelam <prasamsaneelam118@msitprogram.net>
2018-10-25 20:38:56 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignment/m22/22.1 Ransom Note/SequentialSearchST.java
+++ ADS-1 assignment/m22/22.1 Ransom Note/SequentialSearchST.java
@@ -1,3 +1,31 @@
+/**
+ *  The {@code SequentialSearchST} class represents an (unordered)
+ *  symbol table of generic key-value pairs.
+ *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ *  It also provides a <em>keys</em> method for iterating over all of the keys.
+ *  A symbol table implements the <em>associative array</em> abstraction:
+ *  when associating a value with a key that is already in the symbol table,
+ *  the convention is to replace the old value with the new value.
+ *  The class also uses the convention that values cannot be {@code null}. Setting the
+ *  value associated with a key to {@code null} is equivalent to deleting the key
+ *  from the symbol table.
+ *  <p>
+ *  This implementation uses a singly-linked list and sequential search.
+ *  It relies on the {@code equals()} method to test whether two keys
+ *  are equal. It does not call either the {@code compareTo()} or
+ *  {@code hashCode()} method. 
+ *  The <em>put</em> and <em>delete</em> operations take linear time; the
+ *  <em>get</em> and <em>contains</em> operations takes linear time in the worst case.
+ *  The <em>size</em>, and <em>is-empty</em> operations take constant time.
+ *  Construction takes constant time.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/31elementary">Section 3.1</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
 public class SequentialSearchST<Key, Value> {
     private int n;           // number of key-value pairs
     private Node first;      // the linked list of key-value pairs
@@ -120,4 +148,20 @@ public class SequentialSearchST<Key, Value> {
         x.next = delete(x.next, key);
         return x;
     }
-}
+
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     *
+     * @return all keys in the symbol table
+     */
+    // public Iterable<Key> keys()  {
+    //     Queue<Key> queue = new Queue<Key>();
+    //     for (Node x = first; x != null; x = x.next)
+    //         queue.enqueue(x.key);
+    //     return queue;
+    // }
+
+    }
